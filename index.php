3934df80bf6614b113bd2ec916252040568b50f7
Ivar Nesje <ivarne@gmail.com>
2010-12-18 18:08:32 +0100

    sikkrehetsoppdatering

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 header('Content-Type: text/html; charset=UTF-8');
-include 'menu.php';
+include 'config.php';
+if(!in_array($_GET['tab'],array_keys($menu)))
+exit('feil i tab.');
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
