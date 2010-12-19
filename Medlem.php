e3c555cf9d383a5fded1fe8f2b6a0ed8df7f9da2
Ivar Nesje <ivarne@gmail.com>
2010-12-19 22:22:45 +0100

    hoynet brukervennligheten

---
+++ Medlem.php
@@ -0,0 +1,18 @@
+<h2>Medlemssider</h2>
+<p>Du er ikke logget inn. Vennligst logg inn i skjemaet nedenfor</p>
+<form action="" method="post">
+  <table class="span-7 prepend-5">
+    <tr>
+      <td ><label for="username">Brukernavn:</label></td>
+      <td ><input name="username" id="username" type="text" lenght="30"/></td>
+    </tr>
+    <tr>
+      <td><label for="passwd">Passord:</label></td>
+      <td><input name="passwd" id="passwd" type="password" lenght="30"></td>
+    </tr>
+    <tr>
+      <td></td>
+      <td><input type="submit" value="Log inn"></td>
+    </tr>
+  </table>
+</form>
